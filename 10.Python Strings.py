fb539d19fc2f3353ab4e433721b479723070086e
David Greaves <dj_greaves@msn.com>
2020-08-02 07:02:58 +1000

    exercise complete

---
+++ 10.Python Strings.py
@@ -0,0 +1,14 @@
+my_string = "Hello, word!"
+print(my_string)
+
+multiline = """Hello.
+This is a multiline string
+
+"""
+print(multiline)
+
+name = "Peter"
+age = 34
+greeting = "Hello, " + name
+print(greeting)
+print("You are " + str(age))
