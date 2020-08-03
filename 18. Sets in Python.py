0feb966ed567bf09cda5f96b880ac2843ea6aaa1
David Greaves <dj_greaves@msn.com>
2020-08-03 20:18:00 +1000

    daily changes

---
+++ 18. Sets in Python.py
@@ -0,0 +1,9 @@
+art_friends = {"Rolf", "Anne"}
+science_friends = {"Jenn", "Charlie"}
+
+
+art_friends.add("Stevo")
+
+art_friends.remove("Anne")
+
+print(art_friends)
---
+++ 19. Advanced set operations.py
@@ -0,0 +1,20 @@
+art_friends = {"Rolf", "Anne" , "Jenn"}
+science_friends = {"Jenn", "Charlie"}
+
+art_but_not_science = art_friends.difference(science_friends)
+science_but_not_art = science_friends.difference(art_friends)
+
+print("Art Only: " + str(art_but_not_science))
+print("Science Only: " + str(science_but_not_art))
+
+not_in_both = art_friends.symmetric_difference(science_friends)
+print("Not in both: " + str(not_in_both))
+
+art_and_science = art_friends.intersection(science_friends)
+print("In Both: " + str(art_and_science))
+
+all_friends = art_friends.union(science_friends)
+print("All Friends: " + str(all_friends))
+
+new_friend = input("What is a name of a friend: ")
+user_friends
