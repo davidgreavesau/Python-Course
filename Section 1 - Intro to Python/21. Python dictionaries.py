314281ffa2b14307c9bc88d02348eb9f16a0707e
David Greaves <dj_greaves@msn.com>
2020-08-04 17:23:28 +1000

    check in

---
+++ Section 1 - Intro to Python/21. Python dictionaries.py
@@ -0,0 +1,21 @@
+friend_ages = {"Rolf": 24, "Adam" : 30, "Anne" : 27}
+print(friend_ages["Rolf"])
+
+#Assigning value to a key
+friend_ages["Bob"] = 20
+friend_ages["Rolf"] = 25
+print(friend_ages)
+
+friends = (
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27}
+)
+
+print(friends[0]["name"])
+print(friends[1]["name"])
+print(friends[2]["name"])
+
+friends_two = [("Rolf", 24), ("Adam", 30), ("Anne", 27)]
+friends_ages = dict(friends_two)
+print(friends_ages)
