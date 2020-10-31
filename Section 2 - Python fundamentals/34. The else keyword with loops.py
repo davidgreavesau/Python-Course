0baa78e1f941942d54546af17a798f316a8d9f32
David Greaves <dj_greaves@msn.com>
2020-10-31 17:09:23 +1000

    check in

---
+++ Section 2 - Python fundamentals/34. The else keyword with loops.py
@@ -0,0 +1,13 @@
+# https://blog.tecladocode.com/python-more-uses-for-else/
+
+cars = ["ok", "ok", "ok", "faulty", "ok", "ok"]
+
+for status in cars:
+    if status == "faulty":
+        print("Stopping the production line!")
+
+        break
+    print(f"This car is {status}.")
+    print("Shipping new car to customer")
+else:
+    print("All cars built successfully. No faulty cars!")
